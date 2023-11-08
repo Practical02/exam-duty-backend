63531d5e4c218546f599a3f30d89c1dfb04cf201
Amal Manoj <amalmanoj02@gmail.com>
2023-11-08 22:56:10 +0530

    set time zone

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-const express = require('express');
-require('@prisma/client');
+const express = require("express");
+require("@prisma/client");
 const app = express();
-require('dotenv').config();
-const route = require('./routes');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+require("dotenv").config();
+const route = require("./routes");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+async function setDatabaseTimeZone() {
+  try {
+    await prisma.$queryRaw('SET TIME ZONE "Asia/Kolkata";');
+  } catch (error) {
+    console.error("Error setting database time zone:", error);
+  }
+}
 
+setDatabaseTimeZone()
+  .then(() => {
+    console.log("Database time zone set to IST (Indian Standard Time).");
+  })
+  .catch((error) => {
+    console.error("Error setting database time zone:", error);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
 
-
-app.use('/', route);
+app.use("/", route);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+  console.log(`server is running on port ${port}`);
 });
